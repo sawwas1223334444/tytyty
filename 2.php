ccc56a60334185b97204519ca58e836f2a495a62
Karina <KARINA>
2025-03-02 22:31:03 +0300

    Написана вторая задача

---
+++ 2.php
@@ -0,0 +1,21 @@
+<?php 
+function determineTriangleType($a, $b, $c) {
+
+    if ($a == $b && $b == $c) {
+        return "Равносторонний треугольник";
+    }
+
+    if ($a == $b || $a == $c || $b == $c) {
+        return "Равнобедренный треугольник";
+    }
+        return "Не треугольник";
+}
+
+$a = 4;
+$b = 6;
+$c = 6;
+
+$triangleType = determineTriangleType($a, $b, $c);
+
+echo "Треугольник со сторонами $a, $b, $c — это $triangleType.";
+?>
